8ffefb5211c99992cf5a617decd73b933559776f
Alina Grozav <alina.grozav@p4b.ro>
2022-04-29 15:12:23 +0300

    page objects

--- page_objects/m16.js
+++ page_objects/m16.js
@@ -7,23 +7,56 @@ module.exports = {
         inputDiag: '[placeholder="weitere Diagnosen"]',
         datatableDiag: '[class="p-5 position-relative"]',
         searchResultDiag: '[class="datatable-body-cell-label"]',
-        //searchResultDiag: '/html/body/modal-container/div/div/app-search-diagnose-modal/div/ngx-datatable/div/datatable-body/datatable-selection/datatable-scroller/datatable-row-wrapper/datatable-body-row/div[2]/datatable-body-cell/div/span',
         checkboxVo: '[class="d-none d-xl-flex mb-2 patient-type w-100 justify-content-between ng-untouched ng-pristine ng-valid"] > div:nth-child(7)',
-        inputHinweis: '[class="my-4"] [placeholder="Keine Daten ausgewählt"]',
-        valueIct: '[class="ng-option ng-option-selected ng-option-marked"]',
-        buttonOk: '[class="btn btn-orange px-3"]',
-        inputErsatz: '/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/div/div',
-        valueVerloren: '/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/ng-dropdown-panel/div/div[2]/div[2]',
-        inputTherapie: '/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/div/div/div[2]/input',
-        valueUnterz: '/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/ng-dropdown-panel/div/div[2]/div[2]',
+        
+        inputErsatz: {
+            selector: '/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/div/div',
+            locateStrategy: 'xpath'
+        },
+        
+        valueVerloren: {
+            selector: '/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/ng-dropdown-panel/div/div[2]/div[2]',
+            locateStrategy: 'xpath'
+        },
+        inputHinweis: {
+            selector: '[class="my-4"] [placeholder="Keine Daten ausgewählt"]',
+            localStrategy: 'css'
+        },
+            
+        valueIct: {
+            selector: '/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/ng-dropdown-panel/div/div[2]/div[2]/span',
+            locateStrategy: 'xpath'
+        },
+            buttonOk: '[class="btn btn-orange px-3"]',
+
+        inputTherapie: {
+            selector:'/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/div/div/div[2]/input',
+            locateStrategy: 'xpath'
+        },
+        
+            valueUnterz:{
+                selector:'/html/body/modal-container/div/div/app-additions-modal-component/div/div[1]/app-info-item/div/div/app-select/div/div/ng-select/ng-dropdown-panel/div/div[2]/div[2]',
+                locateStrategy: 'xpath'
+            },
+             
+            
         inputReason: '[class="form-element mb-2"]>[class="row no-gutters"]>[class="col-12"]>[formcontrolname="tenDigitReason"]',
         inputBegruendung: '[class="col-12"]:nth-child(2)>app-input>div>input',
         toggleDataAccepted: '[class="col-2 col-xs-12 col-sm-12 col-xl-2 d-flex pl-xl-0 mt-xl-0 align-items-xl-center justify-content-center"]>[class="order-section pl-xl-0 mt-xs-4 text-xl-left w-100"]>[formcontrolname="dataAccepted"]',
         toggleDataRecommended: '[class="col-2 col-xs-12 col-sm-12 col-xl-2 d-flex pl-xl-0 mt-xl-0 align-items-xl-center justify-content-center"]>[class="order-section pl-xl-0 mt-xs-4 text-xl-left w-100"]>[formcontrolname="dataRecommended"]',
         dropdownLe: '[class="ng-input"]>input',
-        valueLe: '/html/body/app-root/app-regulation/div/div/div/div[2]/div/app-m16/div/div/form/div/div[2]/div/div[1]/app-select/div/div/ng-select/ng-dropdown-panel/div/div[2]/div[1]/span',
+        valueLe: {
+            selector: '/html/body/app-root/app-regulation/div/div/div/div[2]/div/app-m16/div/div/form/div/div[2]/div/div[1]/app-select/div/div/ng-select/ng-dropdown-panel/div/div[2]/div[1]/span',
+            locateStrategy: 'xpath'
+        },
+        
         buttonSend: '[class="btn btn-red btn-lg"]',
-        resultLe: '/html/body/modal-container/div/div/app-multiple-le-modal/div/div/ngx-datatable/div/datatable-body/datatable-selection/datatable-scroller/datatable-row-wrapper[1]/datatable-body-row/div[2]/datatable-body-cell[2]/div/span',
+
+        resultLe: {
+            selector: '/html/body/modal-container/div/div/app-multiple-le-modal/div/div/ngx-datatable/div/datatable-body/datatable-selection/datatable-scroller/datatable-row-wrapper[1]/datatable-body-row/div[2]/datatable-body-cell[2]/div/span',
+            locateStrategy: 'xpath'
+        },
+        
         buttonOkLe: '[class="btn btn-orange px-5"]',
         buttonConfirm: '[class="btn btn-orange px-5 m-2"]',
         formSubmitted: '[class="py-4"]',
@@ -31,5 +64,31 @@ module.exports = {
         
 
     },
-    commmands: [{}]
+    commmands: [{
+
+        selectValueErsatz: function() {
+            return this
+                .useXpath()
+                .waitForElementPresent('@inputErsatz')
+        },
+
+        selectValueVerloren: function() {
+            return this
+                .useXpath()
+                .waitForElementPresent('@valueVerloren')
+        },
+
+        selectInputHinweis: function() {
+            return this
+                .useCss()
+                .waitForElementPresent('@inputHinweis')
+        },
+
+        selectValueIct: function() {
+            return this
+                .useXpath()
+                .waitForElementPresent('@inputHinweis')
+        },
+        
+    }]
 }
--- tests/evoTest.js
+++ tests/evoTest.js
@@ -3,23 +3,24 @@ module.exports = {
 
     'Evo test case'(browser) {
 
-        const alina = browser.page.login();
+        const login = browser.page.login();
         const patients = browser.page.patientList();
         const m16 = browser.page.m16();
 
-        browser
-            .url('http://evo-dev.hmm.lan/login')
+        login
+            .navigate()
             .maximizeWindow()
-        alina
-            .loginCustom()
 
-            // .waitForElementVisible('@usernameLogin')
-            // .click('@usernameLogin')
-            // .setValue('@usernameLogin', 'asklepiosevo.abenea')
-            // .click('@passwordLogin')
-            // .setValue('@passwordLogin', 'Ungam+20')
-            // .click('@buttonLogin')
-            //.assert.urlContains('regulation')
+        // login
+        //     .loginCustom()
+
+            .waitForElementVisible('@usernameLogin')
+            .click('@usernameLogin')
+            .setValue('@usernameLogin', 'asklepiosevo.abenea')
+            .click('@passwordLogin')
+            .setValue('@passwordLogin', 'Ungam+20')
+            .click('@buttonLogin')
+            .assert.urlContains('regulation')
 
 
         patients
@@ -49,42 +50,41 @@ module.exports = {
             .pause(500)
             .click('@checkboxVo')
             .pause(1000)
-            .click('@inputHinweis')
+            .click('@inputErsatz')
             .pause(1500)
-        // .click('@valueIct')
-        // .useCss().click('@buttonOk')
-        // .pause(1000)
-        // .useXpath().waitForElementVisible('@valueIct')
-        // .pause(1000)
-        // .click('@inputErsatz')
-        // .pause(1000)
-        // .click('@valueVerloren')
-        // .useCss().click('[class="btn btn-orange px-3"]')
-        // .useXpath().click('@inputTherapie')
-        // .pause(1000)
-        // .click('@valueUnterz')
-        // .useCss().click('[class="btn btn-orange px-3"]')
-        // .waitForElementVisible('@inputReason')
-        // .click('@inputBegruendung')
-        // .setValue('@inputBegruendung', 'eve')
-        // .click('toggleDataAccepted')
-        // .click('toggleDataRecommended')
-        // .click('@dropdownLe')
-        // .useXpath().click('@valueLe')
-        // .useCss().click('@buttonSend')
-        // .useXpath().click('@resultLe')
-        // .useCss().waitForElementVisible('@buttonOkLe')
-        // .click('@buttonOkLe')
+            .click('@valueVerloren')
+            .click('@buttonOk')
+            .pause(1000)
+            .click('@inputHinweis')
+            //.pause(1000)
+            .click('@valueIct')
+            .click('@buttonOk')
+            .pause(1000)
+            .click('@inputTherapie')
+            .pause(1000)
+            .click('@valueUnterz')
+            .click('@buttonOk')
+            .waitForElementVisible('@inputReason')
+            .click('@inputBegruendung')
+            .setValue('@inputBegruendung', 'eve')
+            .click('@toggleDataAccepted')
+            .click('@toggleDataRecommended')
+            .click('@dropdownLe')
+            .click('@valueLe')
+            .click('@buttonSend')
+            .click('@resultLe')
+            .waitForElementVisible('@buttonOkLe')
+            .click('@buttonOkLe')
 
 
-        // .click('@buttonConfirm')
-        // .saveScreenshot('tests_output/google.png')
-        // .getText('@formSubmitted', function (result) {
-        //     console.log('result', result);
-        // })
+            .click('@buttonConfirm')
+            .saveScreenshot('tests_output/google.png')
+            .getText('@formSubmitted', function (result) {
+                console.log('result', result);
+            })
 
 
-        // .click('@buttonCloseWindow')
+            .click('@buttonCloseWindow')
 
 
     }
